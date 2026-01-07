dd0d6232f7450388bff6ceefc4e6bf589ffee480
Charlie <160505188+oneandonlycharlie@users.noreply.github.com>
2026-01-08 02:07:11 +0800

    Add function to down load raw csv data from azure

--- ingestor/azure_storage_manager.py
+++ ingestor/azure_storage_manager.py
@@ -1,6 +1,7 @@
 import logging
 from azure.storage.filedatalake import DataLakeFileClient
 import pandas as pd
+import os
 
 def upload_data(data: pd.DataFrame, db_config, pipeline_cfg) -> bool:
     if pipeline_cfg["DATA_EXTRACTION_DATE"]:
@@ -23,4 +24,28 @@ def upload_data(data: pd.DataFrame, db_config, pipeline_cfg) -> bool:
         return True
     except Exception as e:
         logging.error(f"Failed to upload data to Azure Data Lake: {e}")
-        
+
+def download_data(db_config, pipeline_cfg) -> pd.DataFrame:
+    if pipeline_cfg["DATA_EXTRACTION_DATE"]:
+        local_file_path = f"/tmp/downloaded_data_{pipeline_cfg["DATA_EXTRACTION_DATE"]}.csv"
+        file_path = f"rawdata/data_{pipeline_cfg["DATA_EXTRACTION_DATE"]}.csv"
+    else:
+        file_path = pipeline_cfg["FILE_PATH"]
+        local_file_path = "/tmp/downloaded_data.csv"
+    try:
+        file = DataLakeFileClient.from_connection_string(
+            db_config["AZURE_STORAGE_CONNECTION_STRING"], 
+            file_system_name=pipeline_cfg["FILE_SYSTEM_NAME"],
+            file_path=file_path)
+        with open(local_file_path, "wb") as tmp_file:
+            download = file.download_file()
+            download.readinto(tmp_file)
+        print("Successfully downloaded data from Azure Data Lake.")
+        df = pd.read_csv(local_file_path) 
+        return df
+    except Exception as e:
+        logging.error(f"Failed to download data from Azure Data Lake: {e}")
+        return None
+    finally:
+        if os.path.exists(local_file_path):
+            os.remove(local_file_path)       
